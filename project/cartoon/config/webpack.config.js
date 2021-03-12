8fb446b42f30a19d7d3a4f1809bc692d9d5c4b0d
ZainChen <2384439266@qq.com>
2021-03-12 21:38:12 +0800

    update cartoon

--- project/cartoon/config/webpack.config.js
+++ project/cartoon/config/webpack.config.js
@@ -334,6 +334,8 @@ module.exports = function (webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+        // 模块导入别名，指定后可以在文件之直接 import * from 'src/*';
+        src: path.resolve(__dirname, '../src/'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
